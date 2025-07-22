76c0f750e559ba60390fe043fe1d643e2ff7b6ec
Samad05imam <samad1419imam@gmail.com>
2025-07-22 22:39:10 +0530

    updated backend

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-const allowedOrigins = "http://localhost:5173"
-// const allowedOrigins = "https://jobjunction-samad.netlify.app"
+// const allowedOrigins = "http://localhost:5173"
+const allowedOrigins = "https://jobjunction-samad05imam.netlify.app"
 
 const corsOptions = {
   origin: function (origin, callback) {

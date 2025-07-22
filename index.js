d3c22053ee2a9ec5fa1d476d897af68f44de0a4c
Samad05imam <samad1419imam@gmail.com>
2025-07-22 22:01:16 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+dotenv.config();
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+const allowedOrigins = "http://localhost:5173"
+// const allowedOrigins = "https://jobjunction-samad.netlify.app"
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// Routes
+app.get("/" , (req , res)=>{
+  res.send("Hii chela")
+})
+
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+const startServer = async () => {
+  console.log("🚀 Starting server...");
+  try {
+    await connectDB();
+    app.listen(PORT);
+    console.log(`Server connected At "http://localhost/${PORT}"`)
+  } catch (error) {
+    console.error("❌ Failed to start server:", error.message);
+  }
+};
+
+startServer();
